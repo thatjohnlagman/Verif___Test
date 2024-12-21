42bbae16784ea18202c55d288eeef27c3262b6a7
John Ferry Lagman <johnferrydizonlagman@gmail.com>
2024-12-21 14:39:20 +0800

    Fixed icon

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def init_streamlit():
     """Initialize Streamlit page configuration and styling"""
     st.set_page_config(
         page_title="VerifAI: Where AI Meets Authentication",
-        page_icon=os.path.join("images", "logo.png"),  # Or "logo.ico"
+        page_icon=os.path.join("images", "Logo.png"),  
         layout="wide",
         initial_sidebar_state="auto"
     )
